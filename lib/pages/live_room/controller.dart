a1dcf9b526d2b749a18c03962f0581f843cfce2b
My-Responsitories <107370289+My-Responsitories@users.noreply.github.com>
2026-09-13 12:44:12 +0000

    tweaks (#2962)
    * feat: search route
    
    * opt: open more app
    
    * twk
    
    * fix: patch line endings
    
    * twk
    
    * update
    
    Signed-off-by: dom <githubaccount56556@proton.me>
    
    * fix: chooser
    
    * fix: skip route av/bv exact
    
    ---------
    
    Co-authored-by: dom <githubaccount56556@proton.me>

--- lib/pages/live_room/controller.dart
+++ lib/pages/live_room/controller.dart
@@ -603,7 +603,7 @@ class LiveRoomController extends GetxController {
   }
 
   void initDm(LiveDmInfoData info) {
-    if (info.hostList.isNullOrEmpty) {
+    if (info.hostList.isEmpty) {
       return;
     }
     _msgStream =
