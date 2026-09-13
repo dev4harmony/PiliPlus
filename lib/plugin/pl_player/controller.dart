7995811830f74328d72f96f90ef4671950289554
dom <githubaccount56556@proton.me>
2026-09-13 20:54:35 +0800

    fix #2966
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/plugin/pl_player/controller.dart
+++ lib/plugin/pl_player/controller.dart
@@ -1579,9 +1579,6 @@ class PlPlayerController with BlockConfigMixin, AudioNormalizationMixin {
   }
 
   void onSeekEnd() {
-    if (seekToPos != null) {
-      feedBack();
-    }
     if (showSeekPreview) {
       showPreview.value = false;
     }
--- lib/plugin/pl_player/view/view.dart
+++ lib/plugin/pl_player/view/view.dart
@@ -58,6 +58,7 @@ import 'package:PiliPlus/utils/connectivity_utils.dart';
 import 'package:PiliPlus/utils/duration_utils.dart';
 import 'package:PiliPlus/utils/extension/num_ext.dart';
 import 'package:PiliPlus/utils/extension/theme_ext.dart';
+import 'package:PiliPlus/utils/feed_back.dart';
 import 'package:PiliPlus/utils/id_utils.dart';
 import 'package:PiliPlus/utils/image_utils.dart';
 import 'package:PiliPlus/utils/mobile_observer.dart';
@@ -1037,6 +1038,7 @@ class _PLVideoPlayerState extends State<PLVideoPlayer>
 
   void _onHorizontalDragEnd() {
     if (plPlayerController.seekToPos case final seekToPos?) {
+      feedBack();
       plPlayerController
         ..position.value = seekToPos.inSeconds
         ..seekTo(seekToPos, isSeek: false)
