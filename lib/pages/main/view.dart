ec473ea8373f18e72580beba19f8a9392e98aba9
HCha <hcha@hcha.top>
2026-09-14 14:45:57 +0800

    feat(windows): support video URL activation (#2673)
    * feat(windows): support video URL activation
    
    * fix(windows): restore hidden window on URL activation
    
    ---------
    
    Co-authored-by: dom <githubaccount56556@proton.me>

--- lib/pages/main/view.dart
+++ lib/pages/main/view.dart
@@ -98,7 +98,7 @@ class _MainAppState extends PopScopeState<MainApp>
         _handleTray();
       }
     }
-    if (PlatformUtils.isMobile || Platform.isLinux) {
+    if (PlatformUtils.isMobile || Platform.isLinux || Platform.isWindows) {
       PiliScheme.init();
     }
   }
