8f407573fd23d499cc32c5a61d5806e329e21f6e
dom <githubaccount56556@proton.me>
2026-09-18 13:58:17 +0800

    feat: sc to image
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/utils/image_utils.dart
+++ lib/utils/image_utils.dart
@@ -276,11 +276,12 @@ abstract final class ImageUtils {
     required Uint8List bytes,
     required String fileName,
     String ext = 'png',
+    bool showLoading = true,
   }) async {
     SaveResult? res;
     fileName += '.$ext';
     if (PlatformUtils.isMobile || OS.isHarmony) {
-      SmartDialog.showLoading(msg: '正在保存');
+      if (showLoading) SmartDialog.showLoading(msg: '正在保存');
       res = await SaverGallery.saveImage(
         bytes,
         fileName: fileName,
