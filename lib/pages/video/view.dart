cabda65cc03a1e0f330da65fe9aa0423fc861433
hellozeronick <2835103520@qq.com>
2026-09-19 14:26:55 +0800

    sync upstream playIcon position

--- lib/pages/video/view.dart
+++ lib/pages/video/view.dart
@@ -73,8 +73,8 @@ import 'package:PiliPlus/utils/storage_key.dart';
 import 'package:PiliPlus/utils/storage_pref.dart';
 import 'package:PiliPlus/utils/theme_utils.dart';
 import 'package:extended_nested_scroll_view/extended_nested_scroll_view.dart';
-import 'package:flutter/foundation.dart' show kDebugMode, clampDouble;
 import 'package:floating/floating.dart';
+import 'package:flutter/foundation.dart' show kDebugMode, clampDouble;
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
@@ -1434,10 +1434,10 @@ class _VideoDetailPageVState extends State<VideoDetailPageV>
               ],
             ),
           ),
-          const Positioned(
+          Positioned(
             right: 12,
-            bottom: 10,
-            child: PlayIcon(),
+            top: height - 70,
+            child: const PlayIcon(),
           ),
         ],
       );
